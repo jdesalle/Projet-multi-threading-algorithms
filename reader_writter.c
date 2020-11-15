6d8a77e6358d3118982bb8d502d40b1ca9288cef
jdesalle <jonathan.desalle@student.uclouvain.be>
2020-11-15 17:42:45 +0100

    adding file for reader/writter [IN PROGRESS]

---
+++ reader_writter.c
@@ -0,0 +1,9 @@
+/*
+ *Implementation of the reader/writter problem realised for the course "LINFO1252:Computer Systems" given by 
+ *prof E.Riviere at the Université Catholique de Louvain(UCLouvain), in Belgium. This implementation was written by 
+ *pair 3-8 composed of Jonathan de Salle and Philippine de Suraÿ in 2020.
+ */
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
