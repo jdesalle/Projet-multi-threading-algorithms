f259a9f6cc390b244c5e92c48005c1ed0f61732f
desurayp <philippine.desuray@student.uclouvain.be>
2020-11-16 19:34:38 +0100

    Version presque finale prod_cons

--- producer_consummer.c
+++ producer_consummer.c
@@ -2,54 +2,78 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-
-typedef int buffer_item;
+#include <limits.h>
 
 #define slots 8
-#define MAX_PROD 4
-#define MAX_CONS 4
+int MAX_PROD;
+int MAX_CONS;
 
 pthread_mutex_t mutex; 
 sem_t empty;
 sem_t full;
 
+int count_c = 0;
+int count_p = 0;
+
 int first = 0; // index of the first data to consume
 int last = 0; // index of the next data to produce
 
-buffer_item buffer[slots];
+int buffer[slots];
 
-int item = 0;
+int nb_rand(){
+    union {
+        int i;
+        unsigned char uc[sizeof(int)];
+    } u;
+    for (size_t i = 0; i < sizeof(u.uc); i++){
+        u.uc[i] = rand();
+    }
+    return u.i;
+}
 
 void *producer(void* args){
     // insertion
-    for (int i = 0; i < slots; i++){
+    int i = 0;
+    while(i < 1024/MAX_PROD){
         sem_wait(&empty); //producer must wait until one slot is empty
         pthread_mutex_lock(&mutex); //lock buffer
-        buffer[last] = item;
-        printf("The item %d has been produced and placed at the position %d\n", item, last);
+        buffer[last] = nb_rand();
+        printf("The item %d has been produced and placed at the position %d\n", buffer[last], last);
         last = (last+1)%slots; //we move to the next slot
-        item++;   
+        count_p++;
+        while(rand() > RAND_MAX/10000);
         pthread_mutex_unlock(&mutex); //unlock buffer
         sem_post(&full); //wakeup the consumer
+        i++;
     }
     pthread_exit(NULL);
 }
 
 void *consumer(void* args){
     //extraction
-    for(int i = 0; i < slots; i++){
+    int j = 0;
+    while(j < 1024/MAX_CONS){
         sem_wait(&full); //consumer must wait until producer added something
         pthread_mutex_lock(&mutex);
         int x = buffer[first];
         printf("We consumed the item %d at position %d\n", x, first);
-        first = (first+1)%slots; //we move to the next data
+        first = (first+1)%slots;
+        count_c++;
+        while(rand() > RAND_MAX/10000);
         pthread_mutex_unlock(&mutex);
         sem_post(&empty); //wakeup the producer
+        j++;
     }
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
+
+    MAX_CONS = atoi(argv[1]);
+    MAX_PROD = atoi(argv[2]);
+
+    printf("Nombre cons : %d\n", MAX_CONS);
+    printf("Nombre prod : %d\n", MAX_PROD);
     
     // initialize semaphore for empty slots,
     // upon start up all slots are empty
@@ -101,6 +125,9 @@ int main(int argc, char *argv[]){
     if(pthread_mutex_destroy(&mutex)){
         perror("");
     }
+
+    printf("Count_c : %d\n", count_c);
+    printf("Count_p : %d\n", count_p);
 
     return 0;
 }
