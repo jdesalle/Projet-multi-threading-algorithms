2ce1ed268563c4535763ec847dca96854f048504
Jonathan de Salle <jonathan.desalle@student.uclouvain.be>
2020-11-20 20:46:13 +0100

    choice of variables reader/writers

--- reader_writter.c
+++ reader_writter.c
@@ -7,3 +7,22 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+pthread_mutex_t mreaders;//protect readers and nread
+pthread_mutex_t mwriters;//protect writers and nwrite
+pthread_mutex_t x;//check only one reader use sem_wait at a time
+sem_t wread;//block writers
+sem_t sread;//block readers
+int readers=0;//number of reader currently reading
+int writers=0;//number of writers waiting
+int nread=0;//number of reading already done (shoudl go to 2560)
+int nwrite=0;//number of writing alreadydone.(should go to 640)
+
+void reader(void *args){
+	
+}
+void writers(void *args){
+	
+}
+int maint (int argc, char* argv[]){
+	return 0
+}
