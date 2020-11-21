eede384e0cc6ab3b1db0cf28023516335bbe239b
Jonathan de Salle <jonathan.desalle@student.uclouvain.be>
2020-11-21 23:44:39 +0100

    improve philosophe

--- philosophe.c
+++ philosophe.c
@@ -8,7 +8,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 int n=5;
-
+pthread_mutex_t *chopstick;//actual chopstick array used by the thread
 
 void* philosopher(void* arg);
 //take an argument for number of philosohers (threads) with the option -t
@@ -17,7 +17,7 @@ int main(int argc,char *argv[]){
 	while ((ch=getopt(argc, argv, "t:")) !=  EOF){
 		switch(ch){
 			case 't':
-				n=optarg;
+				n=atoi(optarg);
 				break;
 			default:
 				fprintf(stderr, "invalid argument");
@@ -26,13 +26,25 @@ int main(int argc,char *argv[]){
 	//Two structures: chopstick and phil respectively representing the chopsticks and the philosophers
 	//chopstick will be used for the mutexe's checking that each philosophe can access to his chopstick 
 	//phil will contain each thread representing a philosopher]
-	pthread_mutex_t chopstick[n];
+	pthread_mutex_t chopsticks[n];//this one allow us to create an array of element of size unknown at compile time
+	int err;
+	for (int i=0;i<n;i++){
+		err=pthread_mutex_init( chopsticks[i], NULL);
+		if(err!=0)
+			fprintf(stderr,"Error initializing pthread_mutex");
+	}
+	chopstick=chopsticks;//we link this array to ou static pointer, so each thread can use it
 	pthread_t phil[n];
 	for (int i=0; i<n;i++){
-		int err=pthread_create(&(phil[i]),NULL,philosopher(&i),NULL);
+		err=pthread_create(&(phil[i]),NULL,philosopher(&i),NULL);
 		if(err!=0){
 			fprintf(stderr,"Error creating thread %d\n", i);
 		}
+	}
+	for (int i=0;i<n;i++){
+		err=pthread_mutex_destroy( chopsticks[i]);
+		if(err!=0)
+			fprintf(stderr,"Error destroying pthread_mutex");
 	}
 	return 0;
 }
@@ -42,7 +54,7 @@ void* philosopher(void* arg){
 	int id=*((int *)arg);
 	int right=((id+1)%n);
 	for (int i=0;i<10000;i++){
-		printf("philophe %d is thinking",id);
+		printf("philophe %d is thinking\n",id);
 		//if id is pair, begin with left chopstick, otherwise begin with right (to avoid deadlock)
 		if((id&&0b1)==0){
 			pthread_mutex_lock(&chopstick[id]);
@@ -52,7 +64,7 @@ void* philosopher(void* arg){
 			pthread_mutex_lock(&chopstick[right]);
 			pthread_mutex_lock(&chopstick[id]);
 		}
-		printf("philophe %d is eating",id);
+		printf("philophe %d is eating\n",id);
 		pthread_mutex_unlock(&chopstick[id]);
 		pthread_mutex_unlock(&chopstick[right]);
 	}
